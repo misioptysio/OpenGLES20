219a24efb0f66b1fe635b5cd1bf16384a6532270
PtysioHome <misioptysio@gmail.com>
2015-11-07 10:05:17 +0100

    Change of launch icon

---
+++ app/src/main/res/gfx/cv.bat
@@ -0,0 +1,10 @@
+mkdir mipmap-hdpi
+mkdir mipmap-mdpi
+mkdir mipmap-xhdpi
+mkdir mipmap-xxhdpi
+mkdir mipmap-xxxhdpi
+alchlong ---n -o -+ --+ -Xd192p icon.png mipmap-xxxhdpi/ic_launcher.png
+alchlong ---n -o -+ --+ -Xd144p icon.png mipmap-xxhdpi/ic_launcher.png
+alchlong ---n -o -+ --+ -Xd96p icon.png mipmap-xhdpi/ic_launcher.png
+alchlong ---n -o -+ --+ -Xd72p icon.png mipmap-hdpi/ic_launcher.png
+alchlong ---n -o -+ --+ -Xd48p icon.png mipmap-mdpi/ic_launcher.png
