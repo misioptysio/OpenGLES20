db146744dc42fc53acc57bdced0a8914dcd6a4d0
piotr.plys <landofbluegrass@gmail.com>
2015-11-12 12:42:49 +0100

    Jakis burdel jest tu ciagle

--- app/src/main/java/com/games/GLShader.java
+++ app/src/main/java/com/games/GLShader.java
@@ -1,7 +1,25 @@
 package com.games;
 
+import android.content.Context;
+import android.content.res.Resources;
+
 /**
  * Created by piotr.plys on 2015-11-12.
  */
 public class GLShader {
+
+    private final Resources resources;
+
+    public GLShader(Context context) {
+        resources = context.getResources();
+
+        resources.getString(R.string.dupaJasiu);
+    }
+
+    public String getShader(int id) {
+        return resources.getString(id);
+    }
 }
+
+
+getShader("plastic_red")
